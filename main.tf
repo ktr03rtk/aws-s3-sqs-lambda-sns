876cc2445cb36e5e91b755d0d2f4869e0bb7f46d
ktr03 <ktrsouth@gmail.com>
2021-08-01 18:55:03 +0900

    feature: s3 bucket

--- main.tf
+++ main.tf
@@ -11,11 +11,40 @@ variable "aws_account_id" {}
 variable "region" {}
 variable "environment_name" {}
 
+locals {
+  project_name = "terraform-study-s3-sqs-lambda-sns"
+}
+
 resource "aws_sns_topic" "normal" {
   name = "${var.environment_name}-normal"
+
+  tags = {
+    Name = local.project_name
+  }
 }
 
 resource "aws_sns_topic" "dlq" {
   name = "${var.environment_name}-dlq"
+
+  tags = {
+    Name = local.project_name
+  }
 }
 
+# S3
+resource "aws_s3_bucket" "terraform_study" {
+  bucket = local.project_name
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name = local.project_name
+  }
+}
